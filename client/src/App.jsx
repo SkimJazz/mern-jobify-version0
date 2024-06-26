08e5db8225361c95701083970c9700b58fd05005
SkimJazz <jb.skimjazz@outlook.com>
2024-06-26 18:51:35 +1000

    feat: Add errorElement from react router
    - Implement errorElement in App.jsx using ErrorElement component
    
    Feature allows error message display in 'stats' url path. Can be added to addition paths where loader is present.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -33,6 +33,7 @@ import {action as deleteJobAction} from './pages/DeleteJob';
 import {loader as adminLoader} from './pages/Admin';
 import {action as profileAction} from './pages/Profile';
 import {loader as statsLoader} from './pages/Stats';
+import ErrorElement from "./components/ErrorElement.jsx";
 
 
 
@@ -100,6 +101,7 @@ const router = createBrowserRouter([
             path: 'stats',
             element: <Stats />,
             loader: statsLoader,
+            errorElement: <ErrorElement />,
           },
 
           {
--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -3,14 +3,18 @@ import customFetch from '../utils/customFetch';
 import { useLoaderData } from 'react-router-dom';
 
 
-export const loader = async () => {
-    try {
-        const response = await customFetch.get('/jobs/stats');
-        return response.data;
-    }   catch (error) {
-        return error;
-    }
+// export const loader = async () => {
+//     try {
+//         const response = await customFetch.get('/jobs/stats');
+//         return response.data;
+//     }   catch (error) {
+//         return error;
+//     }
+// };
 
+export const loader = async () => {
+    const response = await customFetch.get('/jobs/stats');
+    return response.data;
 };
 
 
