19aaef6ecb95c80d95feaab3f9115d64037d17e1
SkimJazz <jb.skimjazz@outlook.com>
2024-06-26 17:59:49 +1000

    feat: Add React-Query & Devtools, implement in App.jxs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,9 @@
+// Library Imports
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+// Local Imports
 import {
     HomeLayout,
     Landing,
@@ -32,14 +37,20 @@ import {loader as statsLoader} from './pages/Stats';
 
 
 export const checkDefaultTheme = () => {
-  const isDarkTheme =
-      localStorage.getItem('darkTheme') === 'true'
+  const isDarkTheme = localStorage.getItem('darkTheme') === 'true'
   document.body.classList.toggle('dark-theme', isDarkTheme);
   return isDarkTheme;
 };
-checkDefaultTheme();
+const isDarkThemeEnabled = checkDefaultTheme();
 
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 
 const router = createBrowserRouter([
@@ -128,7 +139,11 @@ const router = createBrowserRouter([
 
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+  );
 };
- 
 export default App;
